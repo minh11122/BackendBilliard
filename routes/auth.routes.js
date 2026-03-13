e3b43e26449ebbc1ab3d2c96257d1a35725885a3
Tuấn Minh <136489572+minh11122@users.noreply.github.com>
2026-03-13 20:24:11 +0700

    create updateprofile

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -10,7 +10,8 @@ const {
   googleAuth,
   registerGoogle,
   loginGoogle,
-  getInforById
+  getInforById,
+  updateProfile
 } = require("../controller/auth/auth.controller");
 const authenticate = require("../middleware/authenticate.middleware");
 const authorize = require("../middleware/authorize.middleware");
@@ -24,6 +25,8 @@ router.post("/auth/login", login);
 router.post("/auth/login/google", loginGoogle);
 router.post("/auth/get-role-name-by-id", getRoleNameById);
 
-router.get("/getprofile", authenticate, authorize("CUSTOMER"),getInforById)
+router.get("/getprofile", authenticate, authorize("CUSTOMER"),getInforById);
+router.post("/updateprofile", authenticate, authorize("CUSTOMER"),updateProfile)
+
 
 module.exports = router;
